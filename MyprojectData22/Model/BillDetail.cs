73e98e27f1283ec9efc56a6fd7f9484fde0a2ccf
Cuonghsht <tranmanhcuonghsht12@gmail.com>
2024-11-06 22:41:36 +0700

    Add project files.

---
+++ MyprojectData22/Model/BillDetail.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyprojectData22.Model
+{
+    public class BillDetail
+    {
+    }
+}
---
+++ MyprojectData22/Model/Cart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyprojectData22.Model
+{
+    public class Cart
+    {
+        public Guid IdCart { get; set; }
+        public Guid IdUser { get; set; }
+        public DateTime NgayTao { get; set; }
+        public DateTime NgayCapNhat { get; set; }
+    }
+}
